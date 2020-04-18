f188b75ba95db24458dc4cfee5add2ae159f34db
Tueloper <ozurumbatochukwu@yahoo.com>
2020-04-18 23:33:44 +0100

    :hatched_chick: settup build files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends Component {
     return (
       <div>
         <nav className="navbar navbar-expand-lg sticky-top navbar-light bg-light">
-          <a className="navbar-brand" href="#">Caption Cards</a>
+          <a className="navbar-brand" href="/">Caption Cards</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
