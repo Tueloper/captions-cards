d8d73da17a5bcc064589be4970104bfdbfc8dc2f
Tueloper <ozurumbatochukwu@yahoo.com>
2020-04-19 13:54:50 +0100

    :jack_o_lantern: Each Page has a Title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 import Captions from './../components/Captions';
 import Form from '../components/CaptionForm';
+import { Helmet } from 'react-helmet';
 
 export class Home extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <Helmet>
+          <meta charSet='utf-8'/>
+          <title>Captions</title>
+        </Helmet>
+        <div className="App">
           <Form setAlert={this.props.setAlert}/>
           <Captions/>
+        </div>
       </div>
+
     )
   }
 }
